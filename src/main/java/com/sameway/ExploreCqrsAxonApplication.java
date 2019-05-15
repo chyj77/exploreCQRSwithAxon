8b9fe481ff68ee3a3fd7962853f8c637870113d2
liukaixuan <lkx123>
2019-05-15 22:43:39 +0800

    init

---
+++ src/main/java/com/sameway/ExploreCqrsAxonApplication.java
@@ -0,0 +1,13 @@
+package com.sameway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCqrsAxonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExploreCqrsAxonApplication.class, args);
+	}
+}
